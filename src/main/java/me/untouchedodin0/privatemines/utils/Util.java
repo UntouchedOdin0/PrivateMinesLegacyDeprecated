a1953584846b260cab49469a4fe12e124dc92a52
UntouchedOdin0 <kylehicksuk@gmail.com>
2021-07-05 17:41:41 +0100

    trying to do directional shit, should've added this to previous commit, oh well.

--- src/main/java/me/untouchedodin0/privatemines/utils/Util.java
+++ src/main/java/me/untouchedodin0/privatemines/utils/Util.java
@@ -1,5 +1,7 @@
 package me.untouchedodin0.privatemines.utils;
 
+import org.bukkit.block.BlockFace;
+
 public class Util {
 
     /*
@@ -7,6 +9,21 @@ public class Util {
      */
 
     // stops it from saying the class is empty.
+
+    public static float getYaw(BlockFace blockFace) {
+        switch (blockFace) {
+            case NORTH:
+                return 180f;
+            case EAST:
+                return -90f;
+            case SOUTH:
+                return -180f;
+            case WEST:
+                return 90f;
+            default:
+                return 0f;
+        }
+    }
 
     @Override
     public String toString() {
