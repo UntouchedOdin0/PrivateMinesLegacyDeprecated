e1506b9f6e757edff77339c50dc361d593eca2e4
UntouchedOdin0 <kylehicksuk@gmail.com>
2021-07-07 16:59:24 +0100

    lets fix the default location stuffs

--- src/main/java/me/untouchedodin0/privatemines/world/MineWorldManager.java
+++ src/main/java/me/untouchedodin0/privatemines/world/MineWorldManager.java
@@ -1,27 +1,24 @@
 package me.untouchedodin0.privatemines.world;
 
-import me.untouchedodin0.privatemines.world.utils.Direction;
 import org.bukkit.*;
-
 import static me.untouchedodin0.privatemines.world.utils.Direction.NORTH;
 
 public class MineWorldManager {
-
     private final World minesWorld;
     private final Location defaultLocation;
     private final int borderDistance;
     private int distance = 0;
-    private Direction direction;
-    private Location location;
+    private me.untouchedodin0.privatemines.world.utils.Direction direction;
 
     public MineWorldManager() {
         this.minesWorld = Bukkit.createWorld(
                 new WorldCreator("privatemines")
                         .type(WorldType.FLAT)
                         .generator(new EmptyWorldGenerator()));
-        this.defaultLocation = new Location(minesWorld, 0, 0, 0);
+
         this.borderDistance = 150;
         this.direction = NORTH;
+        defaultLocation = new Location(minesWorld, 0, 0, 0);
     }
 
     public World getMinesWorld() {
@@ -34,13 +31,34 @@ public class MineWorldManager {
             return defaultLocation;
         }
 
-        if (direction == null) {
-            direction = NORTH;
-            location = direction.addTo(defaultLocation, distance * borderDistance);
-            direction = direction.next();
-            if (direction == NORTH)
-                distance++;
+        if (direction == null) direction = NORTH;
+        Location loc = direction.addTo(defaultLocation, distance * borderDistance);
+        direction = direction.next();
+        if (direction == NORTH) distance++;
+        return loc;
+    }
+
+    public enum Direction {
+        NORTH(0, -1), NORTH_EAST(1, -1),
+        EAST(1, 0), SOUTH_EAST(1, 1),
+        SOUTH(0, 1), SOUTH_WEST(-1, 1),
+        WEST(-1, 0), NORTH_WEST(-1, -1);
+
+        private final int xMulti;
+        private final int zMulti;
+
+        Direction(int xMulti, int zMulti) {
+            this.xMulti = xMulti;
+            this.zMulti = zMulti;
         }
-        return location;
+
+        Direction next() {
+            return values()[(ordinal() + 1) % (values().length)];
+        }
+
+        Location addTo(Location loc, int value) {
+
+            return loc.clone().add(value * (double) xMulti, 0, value * (double) zMulti);
+        }
     }
 }
