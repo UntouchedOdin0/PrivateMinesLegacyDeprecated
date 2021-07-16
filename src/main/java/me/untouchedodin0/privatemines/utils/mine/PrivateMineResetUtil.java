afef2c6407ac9e0166a0777403b2fc75a073c463
UntouchedOdin0 <kylehicksuk@gmail.com>
2021-07-16 21:05:29 +0100

    Sorting out the mine reset tasks

---
+++ src/main/java/me/untouchedodin0/privatemines/utils/mine/PrivateMineResetUtil.java
@@ -0,0 +1,51 @@
+package me.untouchedodin0.privatemines.utils.mine;
+
+import me.untouchedodin0.privatemines.PrivateMines;
+import me.untouchedodin0.privatemines.utils.filling.MineFillManager;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.io.File;
+import java.util.UUID;
+
+public class PrivateMineResetUtil extends BukkitRunnable {
+
+    private BukkitTask bukkitTask;
+    MineFillManager mineFillManager;
+    UUID playerId;
+    File userFile;
+    YamlConfiguration mineConfig;
+
+    public PrivateMineResetUtil(PrivateMines privateMines) {
+        mineFillManager = new MineFillManager(privateMines);
+    }
+
+    public void startResetTask(PrivateMines privateMines, UUID uuid) {
+        this.playerId = uuid;
+
+        if (bukkitTask == null && playerId != null) {
+            Bukkit.getLogger().info("Starting reset task for UUID: " + playerId);
+            bukkitTask = this.runTaskTimer(
+                    privateMines,
+                    0L,
+                    5 * 20L); // Should fix the lag?
+        }
+    }
+
+    @Override
+    public synchronized void cancel() {
+        if (bukkitTask != null) {
+            bukkitTask.cancel();
+        }
+    }
+
+    @Override
+    public void run() {
+        Bukkit.broadcastMessage("reset task, pog!");
+        userFile = new File("plugins/PrivateMinesRewrite/mines/" + playerId + ".yml");
+        mineConfig = YamlConfiguration.loadConfiguration(userFile);
+        mineFillManager.fillPlayerMine(playerId);
+    }
+}
