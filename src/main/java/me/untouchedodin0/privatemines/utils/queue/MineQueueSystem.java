b7cfd31d18fe85ca1a38277217e27e0f30f6b32d
UntouchedOdin0 <kylehicksuk@gmail.com>
2021-07-17 13:19:08 +0100

    Added comments Instead of using add I'm using offer to try and stop exceptions.

--- src/main/java/me/untouchedodin0/privatemines/utils/queue/MineQueueSystem.java
+++ src/main/java/me/untouchedodin0/privatemines/utils/queue/MineQueueSystem.java
@@ -11,21 +11,42 @@ public class MineQueueSystem {
 
     final Queue<UUID> mineQueue = new LinkedList<>();
 
+    /*
+        Adds a player into the mine queue because they decided to p2w and get a private mine
+     */
+
     public void queueMineCreation(Player player) {
-        mineQueue.add(player.getUniqueId());
+        mineQueue.offer(player.getUniqueId());
     }
+
+    /*
+        The plugin has finally created the mine, let's remove them from the list.
+     */
 
     public void removeFromQueue(Player player) {
         mineQueue.remove(player.getUniqueId());
     }
 
+    /*
+        Let's check if a player is still in the queue for a mine, they'll likely have a shorter
+        time to get a mine than to get a graphics card. FUCK YOU SCALPERS.
+     */
+
     public boolean queueContainsPlayer(Player player) {
         return mineQueue.contains(player.getUniqueId());
     }
 
+    /*
+        This is a way to get the fucking mine queue... idk why it's here but fuck it.
+     */
+
     public Queue<UUID> getMine_queue() {
         return mineQueue;
     }
+
+    /*
+        This returns how many people are waiting in the queue to get their fucking mines made
+     */
 
     public int getQueueSize() {
         return mineQueue.size();
@@ -47,5 +68,9 @@ public class MineQueueSystem {
             }
         }
         return count;
+    }
+
+    public UUID getNextMine() {
+        return mineQueue.peek();
     }
 }
