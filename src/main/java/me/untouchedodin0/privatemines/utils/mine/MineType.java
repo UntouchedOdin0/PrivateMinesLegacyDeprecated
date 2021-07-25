cd07e0260fb5f0ffbea152769b16e0110ffcec7f
UntouchedOdin0 <kylehicksuk@gmail.com>
2021-07-25 19:54:52 +0100

    Updated the license & added some new varibles and methods

--- src/main/java/me/untouchedodin0/privatemines/utils/mine/MineType.java
+++ src/main/java/me/untouchedodin0/privatemines/utils/mine/MineType.java
@@ -1,7 +1,28 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2021 Kyle Hicks
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package me.untouchedodin0.privatemines.utils.mine;
 
 import org.bukkit.Material;
-import redempt.redlib.configmanager.ConfigManager;
 import redempt.redlib.multiblock.MultiBlockStructure;
 
 import java.io.File;
@@ -10,15 +31,12 @@ import java.util.Map;
 public class MineType {
 
     private MultiBlockStructure multiBlockStructure;
+    private File file;
     private final String type;
-    private final Map<Material, Double> materials;
-    private final File file;
+    private Map<Material, Double> materials;
 
-    public MineType(MultiBlockStructure multiBlockStructure, String mineType, File file) {
-        this.multiBlockStructure = multiBlockStructure;
+    public MineType(String mineType) {
         this.type = mineType;
-        this.materials = ConfigManager.map(Material.class, Double.class);
-        this.file = file;
     }
 
     public MultiBlockStructure getMultiBlockStructure() {
@@ -29,6 +47,14 @@ public class MineType {
         this.multiBlockStructure = multiBlockStructure;
     }
 
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
     public String getMineType() {
         return type;
     }
@@ -37,7 +63,7 @@ public class MineType {
         return materials;
     }
 
-    public File getFile() {
-        return file;
+    public void setMaterials(Map<Material, Double> materials) {
+        this.materials = materials;
     }
 }
