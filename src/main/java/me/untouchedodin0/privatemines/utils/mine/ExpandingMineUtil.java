b05d4da2a694b59706e72b595d5ea5da9e9ba4b5
UntouchedOdin0 <kylehicksuk@gmail.com>
2021-07-29 18:00:43 +0100

    trying to tidy this up a bit..

--- src/main/java/me/untouchedodin0/privatemines/utils/mine/ExpandingMineUtil.java
+++ src/main/java/me/untouchedodin0/privatemines/utils/mine/ExpandingMineUtil.java
@@ -3,6 +3,7 @@ package me.untouchedodin0.privatemines.utils.mine;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.BlockFace;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
@@ -46,6 +47,24 @@ public class ExpandingMineUtil {
         }
     }
 
+    public void fillCuboid(CuboidRegion cuboidRegion, Material material) {
+        int xMin = Integer.min(cuboidRegion.getStart().getBlockX(), cuboidRegion.getEnd().getBlockX());
+        int xMax = Integer.max(cuboidRegion.getStart().getBlockX(), cuboidRegion.getEnd().getBlockX());
+        int yMin = Integer.min(cuboidRegion.getStart().getBlockY(), cuboidRegion.getEnd().getBlockY());
+        int yMax = Integer.max(cuboidRegion.getStart().getBlockY(), cuboidRegion.getEnd().getBlockY());
+        int zMin = Integer.min(cuboidRegion.getStart().getBlockZ(), cuboidRegion.getEnd().getBlockZ());
+        int zMax = Integer.max(cuboidRegion.getStart().getBlockZ(), cuboidRegion.getEnd().getBlockZ());
+        World world = cuboidRegion.getWorld();
+
+        for (int x = xMin; x <= xMax; x++) {
+            for (int y = yMin; x <= yMax; y++) {
+                for (int z = zMin; x <= zMax; z++) {
+                    world.getBlockAt(x, y, z).setType(material);
+                }
+            }
+        }
+    }
+
     public void expandMine(Player player) {
         userFile = new File(MINE_DIRECTORY + player.getUniqueId() + ".yml");
 
@@ -60,26 +79,26 @@ public class ExpandingMineUtil {
         expandRegionBedrock = new CuboidRegion(start, end);
 
         expandRegion.expand(1, 1, 0, 0, 1, 1);
-        expandRegionBedrock.expand(2, 2, 0, 2, 0, 2); //posX before 3
+        expandRegionBedrock.expand(0, 2, 0, 2, 0, 1);
 
         expandRegion.move(0, 0, 0);
-        expandRegionBedrock.move(0, 2, 0);
+//        expandRegionBedrock.move(0, 2, 0);
 
-        expandRegionBedrock.stream().forEach(north -> expandRegionBedrock.getFace(BlockFace.NORTH)
-                .stream()
-                .forEach(bedrock -> bedrock.setType(Material.EMERALD_BLOCK, false)));
-        expandRegionBedrock.stream().forEach(east -> expandRegionBedrock.getFace(BlockFace.EAST)
-                .stream()
-                .forEach(bedrock -> bedrock.setType(Material.BONE_BLOCK, false)));
-        expandRegionBedrock.stream().forEach(south -> expandRegionBedrock.getFace(BlockFace.SOUTH)
-                .stream()
-                .forEach(bedrock -> bedrock.setType(Material.DIAMOND_BLOCK, false)));
-        expandRegionBedrock.stream().forEach(west -> expandRegionBedrock.getFace(BlockFace.WEST)
-                .stream()
-                .forEach(bedrock -> bedrock.setType(Material.GOLD_BLOCK, false)));
-        expandRegionBedrock.stream().forEach(west -> expandRegionBedrock.getFace(BlockFace.DOWN)
-                .stream()
-                .forEach(bedrock -> bedrock.setType(Material.GREEN_WOOL, false)));
+        expandRegion.stream().forEach(block -> {
+            block.setType(Material.EMERALD_BLOCK);
+        });
+
+        expandRegionBedrock.stream().forEach(block -> {
+            if (block.isEmpty()) {
+                block.setType(Material.BEDROCK);
+            }
+        });
+
+//        expandRegionBedrock.stream().forEach(block -> {
+//            if (block.isEmpty()) {
+//                block.setType(Material.REDSTONE_BLOCK);
+//            }
+//        });
 
         mineConfig.set(CORNER_1_STRING, expandRegion.getStart());
         mineConfig.set(CORNER_2_STRING, expandRegion.getEnd());
@@ -89,6 +108,55 @@ public class ExpandingMineUtil {
             e.printStackTrace();
         }
     }
+//    public void expandMine(Player player) {
+//        userFile = new File(MINE_DIRECTORY + player.getUniqueId() + ".yml");
+//
+//        if (!userFile.exists()) {
+//            Bukkit.getLogger().info("Failed to upgrade " + player.getName() + "'s mine due to them not owning one");
+//            return;
+//        }
+//        mineConfig = YamlConfiguration.loadConfiguration(userFile);
+//        start = mineConfig.getLocation(CORNER_1_STRING);
+//        end = mineConfig.getLocation(CORNER_2_STRING);
+//        expandRegion = new CuboidRegion(start, end);
+//        expandRegionBedrock = new CuboidRegion(start, end);
+//
+//        expandRegion.expand(1, 1, 0, 0, 1, 1);
+//
+//        expandRegionBedrock.forEachBlock(block -> {
+//            block.setType(Material.AIR, false);
+//        });
+//
+//        expandRegionBedrock.expand(2, 2, 0, 2, 0, 2); //posX before 3
+//
+//        expandRegion.move(0, 0, 0);
+//        expandRegionBedrock.move(0, 2, 0);
+//
+//        fillCuboid(expandRegionBedrock, Material.EMERALD_BLOCK);
+////        expandRegionBedrock.stream().forEach(north -> expandRegionBedrock.getFace(BlockFace.NORTH)
+////                .stream()
+////                .forEach(bedrock -> bedrock.setType(Material.EMERALD_BLOCK, false)));
+////        expandRegionBedrock.stream().forEach(east -> expandRegionBedrock.getFace(BlockFace.EAST)
+////                .stream()
+////                .forEach(bedrock -> bedrock.setType(Material.BONE_BLOCK, false)));
+////        expandRegionBedrock.stream().forEach(south -> expandRegionBedrock.getFace(BlockFace.SOUTH)
+////                .stream()
+////                .forEach(bedrock -> bedrock.setType(Material.DIAMOND_BLOCK, false)));
+////        expandRegionBedrock.stream().forEach(west -> expandRegionBedrock.getFace(BlockFace.WEST)
+////                .stream()
+////                .forEach(bedrock -> bedrock.setType(Material.GOLD_BLOCK, false)));
+////        expandRegionBedrock.stream().forEach(west -> expandRegionBedrock.getFace(BlockFace.DOWN)
+////                .stream()
+////                .forEach(bedrock -> bedrock.setType(Material.GREEN_WOOL, false)));
+//
+//        mineConfig.set(CORNER_1_STRING, expandRegion.getStart());
+//        mineConfig.set(CORNER_2_STRING, expandRegion.getEnd());
+//        try {
+//            mineConfig.save(userFile);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 //    public void expandMine(Player player) {
 //        userFile = new File(MINE_DIRECTORY + player.getUniqueId() + ".yml");
