b721a64b339251b4e5ce24b58ccfd7aaadf648e9
UntouchedOdin0 <kylehicksuk@gmail.com>
2021-08-03 15:19:15 +0100

    more unused code being removed

--- src/main/java/me/untouchedodin0/privatemines/guis/BannedPlayersGui.java
+++ src/main/java/me/untouchedodin0/privatemines/guis/BannedPlayersGui.java
@@ -23,9 +23,7 @@
 package me.untouchedodin0.privatemines.guis;
 
 import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
-//import net.md_5.bungee.api.chat.hover.content.Text;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.YamlConfiguration;
