398c9b5a27d952cff9775883fcfc905be3e10cd1
UntouchedOdin0 <kylehicksuk@gmail.com>
2021-08-05 22:22:45 +0100

    creating the MineTypeLoader class

---
+++ src/main/java/me/untouchedodin0/privatemines/utils/mine/MineTypeLoader.java
@@ -0,0 +1,12 @@
+package me.untouchedodin0.privatemines.utils.mine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MineTypeLoader {
+
+    public void loadMineTypes(MineType mineType) {
+        List<MineType> mineTypes = new ArrayList<>();
+        mineTypes.add(mineType);
+    }
+}
