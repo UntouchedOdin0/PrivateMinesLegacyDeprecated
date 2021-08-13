2c6c6fed38b29edcf6abd5a6c329c1455357fc4f
UntouchedOdin0 <kylehicksuk@gmail.com>
2021-08-13 17:58:15 +0100

    Added some null checks

--- src/main/java/me/untouchedodin0/privatemines/commands/PrivateMinesCmd.java
+++ src/main/java/me/untouchedodin0/privatemines/commands/PrivateMinesCmd.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import redempt.redlib.commandmanager.CommandHook;
 import redempt.redlib.commandmanager.Messages;
 
+@SuppressWarnings("unused")
 public class PrivateMinesCmd {
 
     MineStorage mineStorage;
@@ -19,13 +20,17 @@ public class PrivateMinesCmd {
 
     @CommandHook("give")
     public void giveCommand(CommandSender sender, Player target) {
-        mineFactory.createMine(target);
-        Messages.msg("recievedMine");
+        if (sender != null) {
+            mineFactory.createMine(target);
+            Messages.msg("recievedMine");
+        }
     }
 
     @CommandHook("delete")
     public void deleteCommand(CommandSender sender, Player target) {
-        mineFactory.deleteMine(target);
-        Messages.msg("deletedMine");
+        if (sender != null) {
+            mineFactory.deleteMine(target);
+            Messages.msg("deletedMine");
+        }
     }
 }
