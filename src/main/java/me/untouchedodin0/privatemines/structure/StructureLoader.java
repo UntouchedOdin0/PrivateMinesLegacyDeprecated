986b018b5d1a871e39cd87049eada7faf460f726
UntouchedOdin0 <kylehicksuk@gmail.com>
2021-08-14 13:33:19 +0100

    removed old variables

--- src/main/java/me/untouchedodin0/privatemines/structure/StructureLoader.java
+++ src/main/java/me/untouchedodin0/privatemines/structure/StructureLoader.java
@@ -1,19 +1,12 @@
 package me.untouchedodin0.privatemines.structure;
 
 import me.untouchedodin0.privatemines.utils.mine.loop.MineLoopUtil;
-import org.bukkit.Location;
 import redempt.redlib.multiblock.MultiBlockStructure;
 
 public class StructureLoader {
 
     MineLoopUtil mineLoopUtil;
     MultiBlockStructure blockStructure;
-    private int[][] cornerLocations;
-    private int[] dimensions;
-    private Location startLocation;
-    private Location endLocation;
-    private Location spawnLocation;
-    private Location npcLocation;
 
     public StructureLoader(MineLoopUtil mineLoopUtil) {
         this.mineLoopUtil = mineLoopUtil;
@@ -21,9 +14,5 @@ public class StructureLoader {
 
     public void loadStructure(MultiBlockStructure multiBlockStructure) {
         this.blockStructure = multiBlockStructure;
-//        this.cuboidRegion = blockStructure.getRegion(getStart());
-//        setSpawnLocation(mineLoopUtil.findSpawnPointLocation(cuboidRegion.getStart(), cuboidRegion.getEnd(), spawnMaterial));
-//        setNpcLocation(mineLoopUtil.findNpcLocation(cuboidRegion.getStart(), cuboidRegion.getEnd(), npcMaterial));
-//        setCornerLocations(mineLoopUtil.findCornerLocations(blockStructure, cornerMaterial));
     }
 }
