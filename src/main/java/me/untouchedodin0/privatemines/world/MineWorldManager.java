5defe07f182ca94960781eb069f15af85e28bfc5
UntouchedOdin0 <kylehicksuk@gmail.com>
2021-08-22 15:47:19 +0100

    Removed unused Direction from MineWorldManager

--- src/main/java/me/untouchedodin0/privatemines/world/MineWorldManager.java
+++ src/main/java/me/untouchedodin0/privatemines/world/MineWorldManager.java
@@ -59,28 +59,4 @@ public class MineWorldManager {
         if (direction == NORTH) distance++;
         return loc;
     }
-
-    public enum Direction {
-        NORTH(0, -1), NORTH_EAST(1, -1),
-        EAST(1, 0), SOUTH_EAST(1, 1),
-        SOUTH(0, 1), SOUTH_WEST(-1, 1),
-        WEST(-1, 0), NORTH_WEST(-1, -1);
-
-        private final int xMulti;
-        private final int zMulti;
-
-        Direction(int xMulti, int zMulti) {
-            this.xMulti = xMulti;
-            this.zMulti = zMulti;
-        }
-
-        Direction next() {
-            return values()[(ordinal() + 1) % (values().length)];
-        }
-
-        Location addTo(Location loc, int value) {
-
-            return loc.clone().add(value * (double) xMulti, 0, value * (double) zMulti);
-        }
-    }
 }
